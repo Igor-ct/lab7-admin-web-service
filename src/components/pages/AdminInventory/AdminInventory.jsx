15be8cb68bbfeb7a04f0aef924a98010fd3a041b
Igor <igortsebrovski2007@gmail.com>
2026-04-14 15:21:59 +0300

    feat: create page stubs and extract Dashboard component

---
+++ src/components/pages/AdminInventory/AdminInventory.jsx
@@ -0,0 +1,10 @@
+function AdminInventory() {
+  return (
+    <div>
+      <h2>Inventory List</h2>
+      <p>Table with items will be here...</p>
+    </div>
+  );
+}
+
+export default AdminInventory;
---
+++ src/components/pages/AdminInventoryDetails/AdminInventoryDetails.jsx
@@ -0,0 +1,10 @@
+function AdminInventoryDetails() {
+  return (
+    <div>
+      <h2>Item Details</h2>
+      <p>Full information about specific position...</p>
+    </div>
+  );
+}
+
+export default AdminInventoryDetails;
